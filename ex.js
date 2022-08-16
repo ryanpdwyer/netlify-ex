2f897689b67e834512faa8c079a58bbd3b1a0867
Ryan Dwyer <ryanpdwyer@gmail.com>
2022-08-16 16:34:53 -0400

    Updated example

--- ex.js
+++ ex.js
@@ -1,3 +1,8 @@
-import { NetlifyAPI } from 'netlify'
+
+import {NetlifyAPI} from 'netlify';
 
 const client = new NetlifyAPI('AwXJ-jyOxtWjtbDogyY47R-kLhrVaJes4G_0ruxkAYs')
+
+const sites = await client.listFormSubmissions("120-water-22fa")
+
+console.log(sites)
